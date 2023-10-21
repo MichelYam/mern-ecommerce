c2a91db3016ac769b7c0e23354d5d2a5a691e64f
MichelYAM <michel.yam92@gmail.com>
2023-10-21 17:00:03 +0200

    add frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,53 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Login from './pages/Auth/Login';
-import Register from './pages/Auth/Register';
-import Home from './pages/Home';
-import CheckOut from './pages/CheckOut';
-import Error from './pages/Error';
+import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import Product from './pages/Product';
-import Footer from './components/Footer';
-import { setToken } from './utils/TokenStorage';
-import Profile from './pages/Profile';
-import Forgot from './pages/Forgot';
-import ResetPassword from './pages/ResetPassword';
 
-
-// Authentication
-const token = localStorage.getItem('token');
-
-if (token) {
-  // authenticate api authorization
-  setToken(token);
-
-  // authenticate routes
-  // store.dispatch({ type: SET_AUTH });
-}
 function App() {
   return (
-    <div className='container'>
-      <BrowserRouter>
-        <Header />
-        <main>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-            {/* <Route element={<ProtectedRoute />}>
-        </Route> */}
-            <Route path="/forgot" element={<Forgot />} />
-            <Route path="/reset/:token" element={<ResetPassword />} />
-            <Route path="/account" element={<Profile />} />
-            <Route path="/products/:id" element={<Product />} />
-            <Route path="/checkout" element={<CheckOut />} />
-            <Route path='/404' element={<Error />} />
-            <Route path="*" element={<Error />} />
-          </Routes>
-        </main>
-        <Footer />
-      </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- frontend/src/pages/Error/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const index = () => {
-    return (
-        <div>Error 404</div>
-    )
-}
-
-export default index
